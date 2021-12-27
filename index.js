9eb833638445c90e607cba8a8894c8d54b37548a
Liam <hashtyphoon@protonmail.com>
2021-12-27 05:27:42 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu, Tray  } = require("electron");
+var tray = null;
 
 function initWindow() {
   let mainView = new BrowserWindow({
     height: 600,
     width: 800,
-    icon: "build/icon.png",
-    frame: true,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -13,11 +12,45 @@ function initWindow() {
     },
     autoHideMenuBar: true,
   });
-  mainView.loadFile("webpage/index.html");
+  mainView.loadFile("src/index.html");
   mainView.setResizable(false);
-  mainView.on("closed", function () {
-    mainView = null;
-  });
+
+  mainView.on('minimize',function(event){
+    event.preventDefault();
+    mainView.hide();
+});
+  tray = new Tray("./assets/icon.png")
+  const contextMenu = Menu.buildFromTemplate([
+    {
+        label: "Last.fm Rich Presence",
+        enabled: false,
+      },
+      {
+        type: "separator",
+      },
+      {
+        label: "Open App",
+        click: function () {
+          mainView.show();
+        },
+      },
+      {
+        label: "Quit",
+        click: function () {
+          app.quit();
+        },
+      },
+    ])
+  tray.setToolTip('Last.fm Discord Rich Presence')
+  tray.setContextMenu(contextMenu)
 }
 
-app.on("ready", initWindow);
+app.whenReady().then(() => {
+    initWindow()
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) initWindow()
+    })
+  })
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
